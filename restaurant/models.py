0d78763ef98e505111f64c4700c6beeac6212730
Rotimi Olanrewaju Clement <rolament4real@gmail.com>
2023-12-14 00:19:13 +0100

    .

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guest = models.SmallIntegerField(default=6)
+    booking_date = models.DateField()
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.FloatField(null=False)
+    inventory = models.IntegerField(default=5)    
+    
+    def __str__(self):
+        return self.title
